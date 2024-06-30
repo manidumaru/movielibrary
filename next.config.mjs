71d8593cb659502dd55b2c03acf65a72e8ceb992
manidumaru <manidumaru36@gmail.com>
2024-06-30 08:58:54 +0545

    env setup

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["image.tmdb.org"], // Replace with your image host domain
   },
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+  },
 };
 
 export default nextConfig;
